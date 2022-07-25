49719ca40cae5c90e4681ddad61f3a096b2ab970
MattFofo <matteo.segattini2@gmail.com>
2022-07-25 11:12:46 +0200

    PageController Index + Create

--- Controllers/PageController.cs
+++ Controllers/PageController.cs
@@ -1,4 +1,5 @@
-﻿using CSM_NET.Models;
+﻿using CSM_NET.Db_context;
+using CSM_NET.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -15,7 +16,37 @@ namespace CSM_NET.Controllers
 
         public IActionResult Index()
         {
+            using(CMSContext ctx = new CMSContext())
+            {
+                List<Page> pages = ctx.pages.ToList();
+
+                return View(pages);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Page page)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(page);
+            }
+            
+            using(CMSContext ctx = new CMSContext())
+            {
+                ctx.pages.Add(page);
+                ctx.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(Index));
+           
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
